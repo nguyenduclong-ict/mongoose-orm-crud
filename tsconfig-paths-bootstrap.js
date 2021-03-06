15f558e67b0219cd629faebe9c524dcb301d77c0
Nguyen Duc Long <nguyenduclong.ict@gmail.com>
2021-03-06 17:37:10 +0700

    add dotenv

--- tsconfig-paths-bootstrap.js
+++ tsconfig-paths-bootstrap.js
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-require("dotenv").config();
 const tsConfig = require("./tsconfig.json");
 const tsConfigPaths = require("tsconfig-paths");
 const baseUrl = process.env.NODE_ENV === "production" ? "./dist" : "./src";
